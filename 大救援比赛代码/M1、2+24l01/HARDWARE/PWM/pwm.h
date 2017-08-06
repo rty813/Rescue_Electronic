09355d216d7cb4ce2a715e05a08b559a47c17450
czj <1216859517@qq.com>
2017-08-06 14:09:01 +0800

    大救援

---
+++ 大救援比赛代码/M1、2+24l01/HARDWARE/PWM/pwm.h
@@ -0,0 +1,19 @@
+#ifndef __PWM_H
+#define __PWM_H
+#include "sys.h"
+ 
+void TIM_PWM_Init(u16 arr,u16 psc);
+void TIM1_Mode_Config(u16 arr,u16 psc);
+void TIM8_Mode_Config(u16 arr,u16 psc);
+void Speed_con_GPIO_Config(void);
+
+
+//M1
+#define CH1_A PBout(14)
+#define CH1_B PBout(12)
+//M2
+#define CH2_A PBout(15)
+#define CH2_B PBout(13)
+
+
+#endif
